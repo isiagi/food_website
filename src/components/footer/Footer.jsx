d09b249b951b96e2f6e4369d6a03b3635e4d15b0
isiagi <isiagigeofrey0@gmail.com>
2022-07-11 12:33:25 +0300

    partial footer css fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./footer.css";
 
 const Footer = () => {
@@ -7,26 +8,36 @@ const Footer = () => {
         <div className="footer__content">
           <div className="footer__one">
             <h2>SmartFit</h2>
-            <p>We are leading outfit website.</p>
-            <p>All Rights Reserved @ 2022</p>
+            <p style={{ color: "#005B99" }}>We are leading outfit website.</p>
+            <p style={{ color: "#FFE4E1" }}>All Rights Reserved @ 2022</p>
           </div>
           <div>
             <h3>Quick Links</h3>
-            <p>Home</p>
+            <Link to="/">
+              <p style={{ color: "#A13E42" }}>Home</p>
+            </Link>
             <p>Cart</p>
           </div>
           <div>
             <h3>Catergories</h3>
-            <p>Sneakers</p>
-            <p>Boots</p>
-            <p>Gentle ware</p>
-            <p>Ladies shoes</p>
+            <Link to="/info/sneakers">
+              <p style={{ color: "#A13E42" }}>Sneakers</p>
+            </Link>
+            <Link to="/info/shoes">
+              <p style={{ color: "#A13E42" }}>Shoes</p>
+            </Link>
+            <Link to="/info/dresses">
+              <p style={{ color: "#A13E42" }}>Dresses</p>
+            </Link>
           </div>
           <div>
             <h3>Contact</h3>
-            <p>+256</p>
-            <p>smartfit@gmail.com</p>
+            <p style={{ color: "#A13E42" }}>+256</p>
+            <p style={{ color: "#A13E42" }}>smartfit@gmail.com</p>
           </div>
+        </div>
+        <div>
+          <p style={{ color: "#FFE4E1" }}>Designed with &#128147; by Isiagi </p>
         </div>
       </div>
     </div>
