759601ea5f7d810a86332e2cedab6ca63c886bcb
isiagi <isiagigeofrey0@gmail.com>
2022-07-20 10:33:52 +0300

    created a local_storage hook

--- src/components/context/Context.js
+++ src/components/context/Context.js
@@ -1,35 +1,5 @@
-import React, { createContext, useState } from "react";
-
-function useLocalStorageState(
-  key,
-  defaultValue = "",
-  { serialize = JSON.stringify, deserialize = JSON.parse } = {}
-) {
-  const [state, setState] = React.useState(() => {
-    const valueInLocalStorage = window.localStorage.getItem(key);
-    if (valueInLocalStorage) {
-      try {
-        return deserialize(valueInLocalStorage);
-      } catch (error) {
-        window.localStorage.removeItem(key);
-      }
-    }
-    return typeof defaultValue === "function" ? defaultValue() : defaultValue;
-  });
-
-  const prevKeyRef = React.useRef(key);
-
-  React.useEffect(() => {
-    const prevKey = prevKeyRef.current;
-    if (prevKey !== key) {
-      window.localStorage.removeItem(prevKey);
-    }
-    prevKeyRef.current = key;
-    window.localStorage.setItem(key, serialize(state));
-  }, [key, state, serialize]);
-
-  return [state, setState];
-}
+import React, { createContext } from "react";
+import useLocalStorageState from '../hooks/useLocalStorage'
 
 export const AppContext = createContext();
 
